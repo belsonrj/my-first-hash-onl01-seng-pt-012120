3c85be9970e6494b77f7d9368348b76780eac92e
Rick Belson <belsonrj@aol.com>
2020-02-01 22:38:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,12 +19,7 @@ def retrieval
 
 end
 
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+def adding(shipping_manifest)
   shipping_manifest[muskets] = "2"
   shipping_manifest[gun powder] = 4
   shipping_manifest
